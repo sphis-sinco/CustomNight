147158fab36a7e4661299172f85ebd720ebb5767
Viwu <sinconsistencia@gmail.com>
2025-12-02 19:38:31 -0600

    footstep sounds

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -16,8 +16,30 @@ class PlayState extends FlxState
 		add(monster);
 		monster.roll_function_fail = rolled ->
 		{
-			if (((rolled / monster.ai_level) * 100) >= 75)
+			var percentage = ((rolled / monster.ai_level) * 100);
+			
+			trace('monster.roll_function_fail');
+			trace('rolled: ' + rolled);
+			trace('monster.ai_level: ' + monster.ai_level);
+			trace('percentage: ' + percentage);
+
+			if (percentage <= 25)
 				FlxG.sound.play('assets/sounds/laugh.wav');
+		};
+		monster.roll_function_success = rolled ->
+		{
+			var percentage = ((rolled / monster.ai_level) * 100);
+			
+			trace('monster.roll_function_success');
+			trace('rolled: ' + rolled);
+			trace('monster.ai_level: ' + monster.ai_level);
+			trace('percentage: ' + percentage);
+
+			if (percentage <= 15)
+				FlxG.sound.play('assets/sounds/footsteps-slow.wav');
+
+			if (percentage >= 75)
+				FlxG.sound.play('assets/sounds/footsteps-fast.wav');
 		};
 	}
 
